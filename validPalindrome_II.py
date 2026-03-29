25261553ebf0d7482f82a219bda101b6126a7b5b
Hana Fathima <hanafathimavp5@gmail.com>
2026-03-29 21:33:36 +0530

    LC60 modified

--- validPalindrome_II.py
+++ validPalindrome_II.py
@@ -1,3 +1,49 @@
+"""
+
+LC: 680. Valid Palindrome II
+
+Easy
+
+Topics
+Mid Level
+Two Pointers
+String
+Greedy
+
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+Example 3:
+
+Input: s = "abc"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+ 
+
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+1,127,819/2.6M
+Acceptance Rate
+44.1%
+
+"""
+
 class Solution:
     def validPalindrome(self, s:str) -> bool:
         def isPalindromeRange(s, left, right):
