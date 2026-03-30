38664913ad4175596d77f499d498ebe25135e674
Hana Fathima <hanafathimavp5@gmail.com>
2026-03-30 20:00:46 +0530

    small updates in LC58

--- lengthOfLastWordInString.py
+++ lengthOfLastWordInString.py
@@ -1,19 +1,46 @@
-# Given a string s consisting of words and spaces, return the length of the last word in the string.
+"""
 
-# A word is a maximal substring consisting of non-space characters only.
+LC: 58. Length of last word
+
+Easy
+
+Topics
+String
+
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
 
  
 
-# Example 1:
+Example 1:
 
-# Input: s = "Hello World"
-# Output: 5
-# Explanation: The last word is "World" with length 5.
-# Example 2:
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
 
-# Input: s = "   fly me   to   the moon  "
-# Output: 4
-# Explanation: The last word is "moon" with length 4.
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.
+ 
+
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+3,349,285/5.7M
+Acceptance Rate
+58.5%
+
+"""
 
 class Solution:
     def lengthOfLastWord(self, s:str) -> int:
