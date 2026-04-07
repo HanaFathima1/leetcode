24fccba82eca8ba077df92d50f3617105804319a
Hana Fathima <hanafathimavp5@gmail.com>
2026-04-07 22:34:42 +0530

    LC14 modified

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,27 +1,31 @@
-# LC:14.LONGEST COMMON PREFIX
+"""
 
-# EASY
+LC:14.LONGEST COMMON PREFIX
 
-# TOPICS:
-# Array
-# String
-# Trie
+EASY
 
-# Write a function to find the longest common prefix string amongst an array of strings.
+TOPICS:
+Array
+String
+Trie
 
-# If there is no common prefix, return an empty string "".
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
 
  
 
-# Example 1:
+Example 1:
 
-# Input: strs = ["flower","flow","flight"]
-# Output: "fl"
-# Example 2:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
 
-# Input: strs = ["dog","racecar","car"]
-# Output: ""
-# Explanation: There is no common prefix among the input strings.
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+"""
 
 def longestcommonprefix(strs):
     if len(strs) == 0:
@@ -32,7 +36,52 @@ def longestcommonprefix(strs):
     for i in range(len(base)):
         for word in strs[1:]:
             if (i==len(word) or word[i] != base[i]):
-                return strs[0:i]
+                return base[0:i]
     return base
+print(longestcommonprefix(strs = ["flower","flow","flight"]))
+print(longestcommonprefix(strs = ["dog","racecar","car"]))
+
+
+"""
+
+Complexity:
+    TC => O(n * m)
+        n = number of strings
+        m = length of the shortest string
+    SC => O(1)
+
+
+"""
+
+
+def longestCommonPrefix(strs):
+    # Step 1: Sort the list of strings
+    strs.sort()
+    
+    # Step 2: Take first and last strings
+    first = strs[0]
+    last = strs[-1]
+    
+    i = 0
+    
+    # Step 3: Compare characters
+    while i < len(first) and first[i] == last[i]:
+        i += 1
+    
+    # Step 4: Return common prefix
+    return first[:i]
+
+"""
+
+Complexity:
+
+TC=>
+    Sorting → O(n log n)
+    Comparison → O(m)
+    Total: O(n log n)
+
+SC => O(1)
+
+"""
     
     
