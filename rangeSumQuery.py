a2367adebe69b28ab5f02f9680f2d558e9089329
Hana Fathima <hanafathimavp5@gmail.com>
2026-06-04 15:53:47 +0530

    LC303 file modified

--- rangeSumQuery.py
+++ rangeSumQuery.py
@@ -1,3 +1,58 @@
+"""
+
+LC: 303. Range Sum Query - Immutable
+
+Easy
+
+Topics
+Array
+Design
+Prefix Sum
+
+Given an integer array nums, handle multiple queries of the following type:
+
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ 
+
+Example 1:
+
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-105 <= nums[i] <= 105
+0 <= left <= right < nums.length
+At most 104 calls will be made to sumRange.
+ 
+
+Seen this question in a real interview before?
+1/6
+Yes
+No
+Accepted
+941,428/1.3M
+Acceptance Rate
+72.3%
+
+"""
+
+
 class NumArray: 
     def __init__(self, nums:list[int]):
         self.prefix_sum = [0]
