6df6f56b2ba9d7432524c64f97ac3fff23c9916e
Hana Fathima <hanafathimavp5@gmail.com>
2026-07-27 20:32:27 +0530

    LC209 ADDED

---
+++ minimumSizeSubarraySum.py
@@ -0,0 +1,65 @@
+"""
+
+LC: 209. Minimum Size Subarray Sum
+
+Medium
+
+Topics
+Array
+Binary Search
+Sliding Window
+Prefix Sum
+
+Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+ 
+
+Example 1:
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+Example 2:
+
+Input: target = 4, nums = [1,4,4]
+Output: 1
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+ 
+
+Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
+ 
+
+Seen this question in a real interview before?
+1/6
+Yes
+No
+Accepted
+2,014,327/3.8M
+Acceptance Rate
+52.4%
+
+"""
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: list[int]) -> int:
+        left=0
+        min_len=float('inf')
+        sums=0
+        for right in range(len(nums)):
+            sums+=nums[right]
+            while sums>=target:
+                min_len=min(min_len,right-left+1)
+                sums-=nums[left]
+                left+=1
+        return min_len if min_len!=float('inf') else 0
+        
